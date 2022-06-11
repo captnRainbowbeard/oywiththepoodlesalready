6a0987971507fe11dfa27158b2390113a3c4155e
g <cantaur3099@gmail.com>
2022-06-11 08:25:49 +0400

    first commit

---
+++ css/main.js
@@ -0,0 +1,9 @@
+import LocomotiveScroll from 'locomotive-scroll';
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth : true ,
+
+});
+
+console.log('jj');
